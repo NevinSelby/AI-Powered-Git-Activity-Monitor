9873562c4c9877e1aeb77c12c822c551a9b6ef3f
NevinSelby <nevinselby2001@gmail.com>
2025-07-27 20:35:22 -0500

    changed path of index.html in tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./client/index.html",
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
